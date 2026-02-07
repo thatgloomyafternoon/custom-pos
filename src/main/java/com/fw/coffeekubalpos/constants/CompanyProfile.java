b65ad0660257096d4ba3c9b251ae351dad3ffac3
fw <thatgloomyafternoon@gmail.com>
2026-02-08 05:21:45 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/constants/CompanyProfile.java
@@ -0,0 +1,10 @@
+package com.fw.coffeekubalpos.constants;
+
+public interface CompanyProfile {
+
+  String name = "Coffee Kubal";
+  String address = "Jl. Cijawura Hilir II No. 206";
+  String city = "Bandung";
+  String phone = "";
+
+}
