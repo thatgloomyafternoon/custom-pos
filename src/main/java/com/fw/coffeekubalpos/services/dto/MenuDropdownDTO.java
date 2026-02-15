055cfbb72fbec5d9de8dd03f6f951d0e5452d84c
fw <thatgloomyafternoon@gmail.com>
2026-02-15 10:16:21 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/services/dto/MenuDropdownDTO.java
@@ -0,0 +1,14 @@
+package com.fw.coffeekubalpos.services.dto;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MenuDropdownDTO {
+
+  private UUID id;
+  private String name;
+
+}
