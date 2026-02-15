0dee5d9d46e4b69679e01c62d9c796c7422cc285
fw <thatgloomyafternoon@gmail.com>
2026-02-15 10:16:18 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/web/responses/MenuDeleteResponse.java
@@ -0,0 +1,12 @@
+package com.fw.coffeekubalpos.web.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MenuDeleteResponse {
+
+  private String message;
+
+}
