ced5e6a70368eb0f764ea0007b4b8acdc095646c
fw <thatgloomyafternoon@gmail.com>
2026-02-17 18:40:25 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/web/responses/OrderDetailsCreateResponse.java
@@ -0,0 +1,12 @@
+package com.fw.coffeekubalpos.web.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDetailsCreateResponse {
+
+  private String message;
+
+}
