7340bfc22d7b4975692394ef3865d5d8c7744f4d
fw <thatgloomyafternoon@gmail.com>
2026-02-23 00:59:39 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/constants/SpringProfileConstants.java
@@ -0,0 +1,8 @@
+package com.fw.coffeekubalpos.constants;
+
+public interface SpringProfileConstants {
+
+  String SPRING_DEFAULT_PROFILE_PROPERTIES = "spring.profiles.default";
+  String DEVELOPMENT = "develop";
+
+}
