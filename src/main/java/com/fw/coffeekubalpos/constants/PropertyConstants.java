9203de2592fe0f4b5cb6268eef8870f2e50058d4
fw <thatgloomyafternoon@gmail.com>
2026-02-23 00:59:25 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/constants/PropertyConstants.java
@@ -0,0 +1,11 @@
+package com.fw.coffeekubalpos.constants;
+
+public interface PropertyConstants {
+
+  String SERVER_PORT = "server.port";
+  String SERVER_SSL_KEY_STORE = "server.ssl.key-store";
+  String SERVER_SERVLET_CONTEXT_PATH = "server.servlet.context-path";
+  String SPRING_APPLICATION_NAME = "spring.application.name";
+  String DATE_TIME_PATTERN = "EEE, dd-MM-yyyy HH:mm";
+
+}
