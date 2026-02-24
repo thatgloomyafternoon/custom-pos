026036df4c7de157bb5d3c67f408989d8bbca266
fw <thatgloomyafternoon@gmail.com>
2026-02-24 17:25:18 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/services/dto/TransactionListingDTO.java
@@ -0,0 +1,19 @@
+package com.fw.coffeekubalpos.services.dto;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TransactionListingDTO {
+
+  private UUID transactionId;
+  private String customerName;
+  private String displayedTransactionDate;
+  private ZonedDateTime transactionDate;
+  private Integer totalPrice;
+  private String totalPriceString;
+
+}
