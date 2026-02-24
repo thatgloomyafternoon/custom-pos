6873afa5434b292fd2c047e03c259a7382878208
fw <thatgloomyafternoon@gmail.com>
2026-02-24 17:25:11 +0700

    progress

---
+++ src/main/java/com/fw/coffeekubalpos/entities/TempAccounts.java
@@ -0,0 +1,11 @@
+package com.fw.coffeekubalpos.entities;
+
+public interface TempAccounts {
+
+  String ACCOUNT1_EMAIL = "user1@mail.com";
+  String ACCOUNT1_PASSWORD = "user1";
+
+  String ACCOUNT2_EMAIL = "user2@mail.com";
+  String ACCOUNT2_PASSWORD = "user2";
+
+}
