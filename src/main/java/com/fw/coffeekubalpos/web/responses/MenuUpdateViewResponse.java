cf13df5302c5d8b03cb8573da378aff36b491c4d
fw <thatgloomyafternoon@gmail.com>
2026-03-03 00:14:53 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/responses/MenuUpdateViewResponse.java
+++ src/main/java/com/fw/coffeekubalpos/web/responses/MenuUpdateViewResponse.java
@@ -1,15 +1,34 @@
 package com.fw.coffeekubalpos.web.responses;
 
 import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class MenuUpdateViewResponse {
 
   private UUID id;
   private String name;
   private Integer price;
 
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
 }
