0b96c759af786256c4368ef54fdf25fbfb4ef387
fw <thatgloomyafternoon@gmail.com>
2026-03-03 00:14:46 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/responses/MenuListingResponse.java
+++ src/main/java/com/fw/coffeekubalpos/web/responses/MenuListingResponse.java
@@ -2,11 +2,7 @@ package com.fw.coffeekubalpos.web.responses;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class MenuListingResponse {
 
   private UUID menuId;
@@ -18,4 +14,67 @@ public class MenuListingResponse {
   private Integer price;
   private String priceString;
 
+  public UUID getMenuId() {
+    return menuId;
+  }
+
+  public void setMenuId(UUID menuId) {
+    this.menuId = menuId;
+  }
+
+  public String getMenuName() {
+    return menuName;
+  }
+
+  public void setMenuName(String menuName) {
+    this.menuName = menuName;
+  }
+
+  public ZonedDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(ZonedDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public String getDisplayedCreatedDate() {
+    return displayedCreatedDate;
+  }
+
+  public void setDisplayedCreatedDate(String displayedCreatedDate) {
+    this.displayedCreatedDate = displayedCreatedDate;
+  }
+
+  public ZonedDateTime getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(ZonedDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public String getDisplayedLastModifiedDate() {
+    return displayedLastModifiedDate;
+  }
+
+  public void setDisplayedLastModifiedDate(String displayedLastModifiedDate) {
+    this.displayedLastModifiedDate = displayedLastModifiedDate;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
+
+  public String getPriceString() {
+    return priceString;
+  }
+
+  public void setPriceString(String priceString) {
+    this.priceString = priceString;
+  }
 }
