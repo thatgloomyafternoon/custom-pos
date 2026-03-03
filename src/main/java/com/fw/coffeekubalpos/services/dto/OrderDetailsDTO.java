2285d65cc9c1779292eaa93393058edd7e054bc6
fw <thatgloomyafternoon@gmail.com>
2026-03-04 00:19:48 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/services/dto/OrderDetailsDTO.java
+++ src/main/java/com/fw/coffeekubalpos/services/dto/OrderDetailsDTO.java
@@ -1,14 +1,32 @@
 package com.fw.coffeekubalpos.services.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class OrderDetailsDTO {
 
   private String menuId;
   private Integer quantity;
   private String notes;
 
+  public String getMenuId() {
+    return menuId;
+  }
+
+  public void setMenuId(String menuId) {
+    this.menuId = menuId;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
 }
