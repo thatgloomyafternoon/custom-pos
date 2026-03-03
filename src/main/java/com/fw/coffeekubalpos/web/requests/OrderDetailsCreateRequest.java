17ca23bd89eaefd854926ad6b9e67e7319702a31
fw <thatgloomyafternoon@gmail.com>
2026-03-04 00:19:44 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/requests/OrderDetailsCreateRequest.java
+++ src/main/java/com/fw/coffeekubalpos/web/requests/OrderDetailsCreateRequest.java
@@ -2,14 +2,25 @@ package com.fw.coffeekubalpos.web.requests;
 
 import com.fw.coffeekubalpos.services.dto.OrderDetailsDTO;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class OrderDetailsCreateRequest {
 
   private String customerName;
   private List<OrderDetailsDTO> orderDetails;
 
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public List<OrderDetailsDTO> getOrderDetails() {
+    return orderDetails;
+  }
+
+  public void setOrderDetails(List<OrderDetailsDTO> orderDetails) {
+    this.orderDetails = orderDetails;
+  }
 }
