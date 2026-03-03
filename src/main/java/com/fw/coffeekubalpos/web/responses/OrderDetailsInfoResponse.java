3e37198bad2572a35db2cc04db80de98ec22545a
fw <thatgloomyafternoon@gmail.com>
2026-03-04 00:19:50 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/responses/OrderDetailsInfoResponse.java
+++ src/main/java/com/fw/coffeekubalpos/web/responses/OrderDetailsInfoResponse.java
@@ -1,15 +1,34 @@
 package com.fw.coffeekubalpos.web.responses;
 
 import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class OrderDetailsInfoResponse {
 
   private UUID orderId;
   private String customerName;
   private String createdDate;
 
+  public UUID getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(UUID orderId) {
+    this.orderId = orderId;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public String getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(String createdDate) {
+    this.createdDate = createdDate;
+  }
 }
