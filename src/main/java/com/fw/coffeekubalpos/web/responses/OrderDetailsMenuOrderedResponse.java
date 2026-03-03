682af13ebb99f5415fb5e1ab8d9729db76f4f8d8
fw <thatgloomyafternoon@gmail.com>
2026-03-04 00:19:52 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/responses/OrderDetailsMenuOrderedResponse.java
+++ src/main/java/com/fw/coffeekubalpos/web/responses/OrderDetailsMenuOrderedResponse.java
@@ -1,11 +1,7 @@
 package com.fw.coffeekubalpos.web.responses;
 
 import java.util.UUID;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class OrderDetailsMenuOrderedResponse {
 
   private UUID orderMenuId;
@@ -15,5 +11,51 @@ public class OrderDetailsMenuOrderedResponse {
   private Integer quantity;
   private String notes;
 
+  public UUID getOrderMenuId() {
+    return orderMenuId;
+  }
 
+  public void setOrderMenuId(UUID orderMenuId) {
+    this.orderMenuId = orderMenuId;
+  }
+
+  public String getMenuName() {
+    return menuName;
+  }
+
+  public void setMenuName(String menuName) {
+    this.menuName = menuName;
+  }
+
+  public String getPriceString() {
+    return priceString;
+  }
+
+  public void setPriceString(String priceString) {
+    this.priceString = priceString;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
 }
