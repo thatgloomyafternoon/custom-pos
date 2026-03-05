03f38f14c061640fe0233ab2812ffba42c4c4a84
fw <thatgloomyafternoon@gmail.com>
2026-03-06 00:45:50 +0700

    progress

--- src/main/java/com/fw/coffeekubalpos/web/responses/TransactionListingResponse.java
+++ src/main/java/com/fw/coffeekubalpos/web/responses/TransactionListingResponse.java
@@ -2,12 +2,8 @@ package com.fw.coffeekubalpos.web.responses;
 
 import com.fw.coffeekubalpos.services.dto.TransactionListingDTO;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
 
 @SuppressWarnings("unused")
-@Getter
-@Setter
 public class TransactionListingResponse {
 
   private List<TransactionListingDTO> transactions;
@@ -15,4 +11,35 @@ public class TransactionListingResponse {
   private Integer totalRevenue;
   private String displayedTotalRevenue;
 
+  public List<TransactionListingDTO> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(List<TransactionListingDTO> transactions) {
+    this.transactions = transactions;
+  }
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  public Integer getTotalRevenue() {
+    return totalRevenue;
+  }
+
+  public void setTotalRevenue(Integer totalRevenue) {
+    this.totalRevenue = totalRevenue;
+  }
+
+  public String getDisplayedTotalRevenue() {
+    return displayedTotalRevenue;
+  }
+
+  public void setDisplayedTotalRevenue(String displayedTotalRevenue) {
+    this.displayedTotalRevenue = displayedTotalRevenue;
+  }
 }
